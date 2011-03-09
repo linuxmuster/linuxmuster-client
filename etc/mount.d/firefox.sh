584a64d17286399b6a95f7c983e2a82436d9145d
linuxmuster <linuxmuster@linuxmuster.net>
2011-03-09 17:15:32 +0000

    moved ff config to mount.d

--- etc/mount.d/firefox.sh
+++ etc/mount.d/firefox.sh
@@ -6,27 +6,47 @@
 # Frank Schiebel
 #
 
-# process firefox profile if requested
-if [ "$FIREFOX" = "yes" ]; then
 
- # create default firefox profile dir if necessary
- [ -d $HOME/.mozilla/firefox ] || mkdir -p $HOME/.mozilla/firefox
+PROFILE_HOME=$(getent passwd $TEMPLATE_USER | awk -F\: '{ print $6 }')
 
- # create profiles.ini with path to custom profile directory
- echo -e "[General]\nStartWithLastProfile=1\n\n[Profile0]\nName=firefox\nIsRelative=1\nPath=../../$APPS_BASEDIR/$FIREFOX_PROFILE\nDefault=1\n\n" > $HOME/.mozilla/firefox/profiles.ini
- chown -R $USER $HOME/.mozilla
+# create default firefox profile dir if necessary
+[ -d $HOME/.mozilla/firefox ] || mkdir -p $HOME/.mozilla/firefox
 
- # create custom firefox profile directory
- if [ ! -d $HOME/$APPS_BASEDIR/$FIREFOX_PROFILE ]; then
+# create profiles.ini with path to custom profile directory
+echo -e "[General]\nStartWithLastProfile=1\n\n[Profile0]\nName=firefox\nIsRelative=1\nPath=../../$APPS_BASEDIR/$FIREFOX_PROFILE\nDefault=1\n\n" > $HOME/.mozilla/firefox/profiles.ini
+chown -R $USER $HOME/.mozilla
 
-  # skip this if user is template user
-  if [ "$USER" != "$TEMPLATE_USER" ]; then
-   [ -e "$PROFILE_HOME/$APPS_BASEDIR/$FIREFOX_PROFILE" ] && cp -a $PROFILE_HOME/$APPS_BASEDIR/$FIREFOX_PROFILE $HOME/$APPS_BASEDIR
-  fi
+# create custom firefox profile directory
+if [ ! -d $HOME/$APPS_BASEDIR/$FIREFOX_PROFILE ]; then
 
-  # create it if it was not copied
-  [ -d $HOME/$APPS_BASEDIR/$FIREFOX_PROFILE ] || mkdir $HOME/$APPS_BASEDIR/$FIREFOX_PROFILE
+# copy profile only ifuser is not the template user and ff profile should be copied
+if [ "$USER" != "$TEMPLATE_USER"  -a "$FIREFOX" = "yes" ]; then
+[ -e "$PROFILE_HOME/$APPS_BASEDIR/$FIREFOX_PROFILE" ] && cp -a $PROFILE_HOME/$APPS_BASEDIR/$FIREFOX_PROFILE $HOME/$APPS_BASEDIR
+fi
 
- fi
+# create it if it was not copied
+[ -d $HOME/$APPS_BASEDIR/$FIREFOX_PROFILE ] || mkdir $HOME/$APPS_BASEDIR/$FIREFOX_PROFILE
 
 fi
+
+# set permissions
+chown -R $USER "$HOME/$APPS_BASEDIR/"
+
+# setting prefs
+prefs="$HOME/$APPS_BASEDIR/$FIREFOX_PROFILE/prefs.js"
+# delete safebrowsing db
+rm "$HOME/$APPS_BASEDIR/$FIREFOX_PROFILE/urlclassifier3.sqlite"
+# deleting prefs if exists
+sed -i "/browser\.cache\.disk\.capacity/d" $prefs
+sed -i "/app\.update\.enabled/d" $prefs
+sed -i "/extensions\.update\.enabled/d" $prefs
+sed -i "/browser\.search\.update/d" $prefs
+sed -i "/browser\.safebrowsing\.enabled/d" $prefs
+sed -i "/browser\.safebrowsing\.malware\.enabled/d" $prefs
+# setting mandatory default values
+echo 'user_pref("browser.cache.disk.capacity",0);' >> $prefs
+echo 'user_pref("app.update.enabled",false);' >> $prefs
+echo 'user_pref("extensions.update.enabled",false);' >> $prefs
+echo 'user_pref("browser.search.update",false);' >> $prefs
+echo 'user_pref("browser.safebrowsing.enabled", false);' >> $prefs
+echo 'user_pref("browser.safebrowsing.malware.enabled", false);' >> $prefs
